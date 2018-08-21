07782b82d4c9b01ca17465e13de3dd08e7694d0f
398855646 <398855646@qq.com>
2018-08-21 15:16:11 +0800

    first Orange Can

---
+++ data/data.js
@@ -0,0 +1,35 @@
+var postList = [{
+  date: "Jan 28 2018",
+  title: "小时候的冰棍与雪糕",
+  postImg: "/images/post/post-4.jpg",
+  avatar: "/images/avatar/avatar-1.png",
+  content: "冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。",
+  readingNum:0,
+  collectionNum:0,
+  commentNum:0
+},
+{
+  date: "Jan 29 2018",
+  title: "小时候的冰棍与雪糕",
+  postImg: "/images/post/post-3.jpg",
+  avatar: "/images/avatar/avatar-2.png",
+  content: "冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。",
+  readingNum: 0,
+  collectionNum: 0,
+  commentNum: 0
+},
+{
+  date: "Jan 30 2018",
+  title: "小时候的冰棍与雪糕",
+  postImg: "/images/post/post-2.jpg",
+  avatar: "/images/avatar/avatar-3.png",
+  content: "冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。冰棍与雪糕绝对不是同一个东西。",
+  readingNum: 0,
+  collectionNum: 0,
+  commentNum: 0
+}
+];
+
+module.exports={
+  postList:postList
+}
